d62f736d8caf19324f59bcf269b4f7381767e494
choway <354988859@qq.com>
2019-01-23 12:15:37 +0800

    init commit.

---
+++ src/main/java/com/choway/wxopenapi/WxopenapiApplication.java
@@ -0,0 +1,14 @@
+package com.choway.wxopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxopenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxopenapiApplication.class, args);
+	}
+
+}
+
